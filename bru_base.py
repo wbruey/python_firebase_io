6b77bc6d0b740e8cdde08b10e4c276cd40129d7e
EC2 Default User <ec2-user@ip-172-31-26-250.us-west-2.compute.internal>
2018-10-28 19:23:02 +0000

    i have successfully used python to write arbitrary data to the firebase realtime database

---
+++ bru_base.py
@@ -0,0 +1,25 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import auth
+from firebase_admin import db
+import argparse
+
+class fb_database:
+
+    def __init__(self):
+        self.cred_cert = credentials.Certificate('brutest-3228a-firebase-adminsdk-bex36-272252f791.json')
+        self.default_app = firebase_admin.initialize_app(self.cred_cert, {'databaseURL':'https://brutest-3228a.firebaseio.com/'})
+
+    def set_data(self,relative_reference,data_dict={}):
+        ref=db.reference(relative_reference)
+        ref.set(data_dict)
+
+
+if __name__ == "__main__":
+    parser=argparse.ArgumentParser(description='CRUD operations on a firebase realtime database at bruey enterprises specifically')
+    parser.add_argument('ref',type=str,help='this is the reference within the mega JSON tree that will act as the root for the input data')
+    parser.add_argument('data',type=dict,help='this is the data you want to write to the firebase realtime database')    
+
+    print('fest')
+    
+    
