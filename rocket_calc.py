2ba661d020345a1dc7b18c675c390b412537bcff
Will Bruey <willbruey@gmail.com>
2018-11-11 22:37:54 +0000

    now the stage object also has a stage_data DataFrame with mass and thrust as a function of time continuously for every milisecond

--- rocket_calc.py
+++ rocket_calc.py
@@ -29,24 +29,95 @@ class vehicle:
             self.wet_mass=self.wet_mass+stage.wet_mass
         logging.info('vehicle has wet mass of ' + str(self.wet_mass) + ' kg ')
 
+
+
+class engine:
+
+    def __init__(self,part_num='F15',delay='0'): # expects there to be an xxx.thrust and an xxx.json to import.  You can get thurst curves here: http://www.thrustcurve.org/simfilesearch.jsp?id=2021
+        try:
+            #open the thrust attributes file and load the static attributes for engine
+            self.part_num=part_num
+            with open(self.part_num+'.attributes') as json_file:
+                    self.attributes=json.load(json_file)
+            self.prop_mass=self.attributes['prop_mass']
+            self.final_mass=self.attributes['final_mass']
+            self.initial_mass=self.attributes[delay]['initial_mass']
+            self.delay=self.attributes[delay]['delay']
+            self.isp=self.attributes['total_impulse']
+            self.thrust_uncertainty_percent=self.attributes['impulse_std_dev_percent']
+
+            #load thrust file and create a time series from it.
+            with open(self.part_num+'.thrust', 'rb') as csvfile:
+                thrust_reader=csv.reader(csvfile,delimiter= ' ')
+                times=[]
+                thrust=[]
+                for row in thrust_reader:
+                    times.append(pd.Timedelta(row[0] +' seconds'))
+                    thrust.append(float(row[1]))
+            self.thrust=pd.Series(thrust,index=times,name='thrust')
+            self.burn_duration=self.thrust.index[-1]-self.thrust.index[0]
+			
+            #create mass time series with the assumption that it will be the same curve shape as thrust file
+            self.specific_impulse=simple_integrate(self.thrust)  #integrate thrust curve to be used in normaization
+            mass_loss=self.thrust/self.specific_impulse*self.prop_mass #normalize thrust curve and multiple by mass loss
+            mass_loss.name='mass_loss'  #name the mass_loss timeseries.  This is the instantanious per/second mass loss at any given time
+            tempdf=pd.DataFrame(mass_loss,columns=['mass_loss'])  #create a temp data frame to find cumulitve mass loss at any given time
+            tempdf['time']=mass_loss.index  #with time so we can do math on it.  Time is a column now so we can convert it to a float to do math to find cumulitve mass loss at any given time
+            tempdf['dtime']=(tempdf['time']-tempdf['time'].shift()).fillna(0).astype('timedelta64[ms]')/1000.0  #another column in the temp data frame is delta time of each timestamp since that's not uniform for a thrust curve
+            self.mass=self.initial_mass-(((tempdf['mass_loss']+tempdf['mass_loss'].shift(-1).fillna(0))/2.0)*tempdf['dtime']).cumsum() #ad up the lsot mass trapazoid stype and subtract it from inital mass
+            self.mass.name='mass'  #name the time series
+                        
+            #put mass and thrust into a milisecond dataframe
+            time_index=pd.timedelta_range(start=pd.Timedelta(value=0,unit='ms') ,end=self.thrust.index[-1],freq='ms') #create a time index of milisecond increments for as long as the thrust (and mass) time series are
+            self.engine_data=pd.DataFrame(self.thrust,index=time_index) #create an engine_data frame with thrust and the time index
+            self.engine_data['mass']=self.mass #add mass to the data_frame
+            self.engine_data=self.engine_data.interpolate() #intermpolate to give us a value for mass and thrust at any given time
+            
+			
+			
+			
+            logging.info('loaded engine ' + part_num + ' prop mass '+str(self.prop_mass)+' final_mass '+str(self.final_mass)+' initial_mass '+str(self.initial_mass))
+        except Exception:
+            logging.exception('Failed to Load Engine')
+
 class stage:
 
-    def __init__(self,dry_mass,engines,cross_section,drag_coeff):  #engines is a list of engines
+    def __init__(self,dry_mass,engines,cross_section,drag_coeff):  #engines is a list of engines  - dry mass is mass of stage with NO engines installed
+        #define the static parametres
         self.dry_mass=dry_mass
         self.engines=engines
         self.cross_section=cross_section
         self.drag_coeff=drag_coeff
         self.wet_mass=self.dry_mass
         self.prop_mass=0
+        self.burn_duration=pd.Timedelta(0,'ms')
         logging.info('stage added with ' + str(self.dry_mass) + ' kg of dry mass and the following engines')
+        
+        #for each engine add the masses to the stage
         for engine in self.engines:
+            self.burn_duration=max(self.burn_duration,engine.burn_duration)
             logging.info(engine.part_num)
             self.wet_mass=self.wet_mass+engine.initial_mass
             self.prop_mass=self.prop_mass+engine.prop_mass
         self.final_mass=self.wet_mass-self.prop_mass
+        
+        #create a data frame "stage_data" with thrust and mass at all times
+        time_index=pd.timedelta_range(start=pd.Timedelta(value=0,unit='ms'), end=self.burn_duration,freq='ms')
+        self.stage_data=pd.DataFrame(index=time_index)
+        self.stage_data['thrust']=np.zeros(len(time_index))
+        self.stage_data['mass']=np.zeros(len(time_index))+self.dry_mass
+        for engine in self.engines:
+            tempDF=pd.DataFrame(engine.engine_data,index=time_index)
+            tempDF['thrust']=tempDF['thrust'].fillna(0)  #if thrust is undefined after the engine data ends, fill it with zeros.
+            tempDF['mass']=tempDF['mass'].interpolate()  #if mass is undefined after the engine data ends, fill it with last value
+            self.stage_data=self.stage_data+tempDF  # add the temp to the stage data
+        
+            
+        
         logging.info('this stage has a '+str(self.cross_section) + ' m^2 cross section and a drag coefficient of ' + str(self.drag_coeff) +' and a total wet mass of ' + str(self.wet_mass) + ' after the engines have burned, this stage will have a final mass of ' + str(self.final_mass))
        
 
+        
 class launch:
 
     def __init__(self,vehicle,sim_duration,density): #density is in kg/m^3  #duration in seconds
@@ -58,7 +129,7 @@ class launch:
         self.initial_conditions['alt1']=0
         self.initial_conditions['vel0']=1
         m0=vehicle.wet_mass
-        cross_section_0=0.0
+        cross_section_0=0.0         
         drag_coeff_0=0.0
         #gather initial conditions from all stages and engines
         for stage in vehicle.stages:
@@ -90,61 +161,5 @@ class launch:
         self.launch_data['vel']=np.zeros(row_count)
         self.launch_data['alt']=np.zeros(row_count)        
         logging.info('blank simulation data structure assembled adding')
-
-
-
-
-
-class engine:
-
-    def __init__(self,part_num='F15',delay='0'): # expects there to be an xxx.thrust and an xxx.json to import.  You can get thurst curves here: http://www.thrustcurve.org/simfilesearch.jsp?id=2021
-        try:
-            #open the thrust attributes file and load the static attributes for engine
-            self.part_num=part_num
-            with open(self.part_num+'.attributes') as json_file:
-                    self.attributes=json.load(json_file)
-            self.prop_mass=self.attributes['prop_mass']
-            self.final_mass=self.attributes['final_mass']
-            self.initial_mass=self.attributes[delay]['initial_mass']
-            self.delay=self.attributes[delay]['delay']
-            self.isp=self.attributes['total_impulse']
-            self.thrust_uncertainty_percent=self.attributes['impulse_std_dev_percent']
-
-            #load thrust file and create a time series from it.
-            with open(self.part_num+'.thrust', 'rb') as csvfile:
-                thrust_reader=csv.reader(csvfile,delimiter= ' ')
-                times=[]
-                thrust=[]
-                for row in thrust_reader:
-                    times.append(pd.Timedelta(row[0] +' seconds'))
-                    thrust.append(float(row[1]))
-            self.thrust=pd.Series(thrust,index=times,name='thrust')
-            self.burn_duration=self.thrust.index[-1]-self.thrust.index[0]
-			
-            #create mass curve from thrust file
-            self.specific_impulse=simple_integrate(self.thrust)
-            mass_loss=self.thrust/self.specific_impulse*self.prop_mass #normalize thrust curve and multiple by mass loss
-            mass_loss.name='mass_loss'
-            tempdf=pd.DataFrame(mass_loss,columns=['mass_loss'])  #create a temp data frame
-            tempdf['time']=mass_loss.index  #with time so we can do math on it.
-            tempdf['dtime']=(tempdf['time']-tempdf['time'].shift()).fillna(0).astype('timedelta64[ms]')/1000.0  #and delta time
-            self.mass=self.initial_mass-(((tempdf['mass_loss']+tempdf['mass_loss'].shift(-1).fillna(0))/2.0)*tempdf['dtime']).cumsum() #ad up the lsot mass trapazoid stype and subtract it from inital mass
-            #self.mass.name='mass'
-                        
-            #put mass and thrust into a milisecond dataframe
-            time_index=pd.timedelta_range(start=pd.Timedelta(value=0,unit='ms') ,end=self.thrust.index[-1],freq='ms')
-            self.engine_data=pd.DataFrame(self.thrust,index=time_index)
-            self.engine_data['mass']=self.mass
-            self.engine_data=self.engine_data.interpolate()          
-            
-			
-			
-			
-            logging.info('loaded engine ' + part_num + ' prop mass '+str(self.prop_mass)+' final_mass '+str(self.final_mass)+' initial_mass '+str(self.initial_mass))
-        except Exception:
-            logging.exception('Failed to Load Engine')
-
-
-        
 
 
--- test_rocket_calc.py
+++ test_rocket_calc.py
@@ -13,11 +13,13 @@ print(bru_solid.specific_impulse)
 print(bru_solid.thrust)
 print(bru_solid.mass)
 print(bru_solid.final_mass)
+print(bru_solid.engine_data)
 
 #kick stage class
 bru_booster=[bru_solid, bru_solid]
 bru_orbiter=[bru_solid]
 stage_1=rc.stage(0.1,bru_booster,.02,.75)
+print(stage_1.stage_data)
 stage_2=rc.stage(0.2,bru_orbiter,.03,.76)
 
 #kick vehicle class
@@ -27,7 +29,7 @@ thanks_giver=rc.vehicle(rocket_assembly)
 #kick launch class
 launch_sim=rc.launch(thanks_giver,30,1)
 
-print(launch_sim.launch_data)
+#print(launch_sim.launch_data)
 
 
 
